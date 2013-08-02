15c7561fd32552c4a649f49cc7840ea883962f38
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2013-08-02 17:23:30 -0300

    split single script into python package

--- arcoiro/__init__.py
+++ arcoiro/__init__.py
@@ -2,7 +2,7 @@
 
 import random
 import requests
-from flask import Flask, abort, make_response, render_template_string
+from flask import Flask, abort, make_response, render_template
 from werkzeug.contrib.cache import FileSystemCache
 
 app = Flask(__name__)
@@ -14,48 +14,6 @@ app.config.from_envvar('ARCOIRO_SETTINGS')
 # store it in cache for 1 day. using file system cache because memcached is
 # too mainstream. :)
 cache = FileSystemCache('/tmp/__arcoiro__', default_timeout=60 * 60 * 24)
-
-FRAMESET = u'''\
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
-<html>
-    <head>
-        <title>arcoiro - faz aquilo que você sabe!</title>
-        <meta http-equiv="cache-control" content="no-cache">
-        <meta http-equiv="pragma" content="no-cache">
-    </head>
-    <frameset rows="*,50" border="0" frameborder="no" framespacing="0">
-        <frame src="{{ url }}" noresize>
-        <frame src="{{ url_for('footer') }}">
-    </frameset>
-</html>
-'''
-
-FOOTER = u'''\
-<html>
-    <head>
-        <style type="text/css">
-            p {
-                font-family: monospace, serif;
-                font-size: 10px;
-                font-weight: bold;
-                margin: 4px auto;
-                text-align: center;
-            }
-        </style>
-    <body>
-        <p>
-            arcoiro - faz aquilo que você sabe! Não entendeu!?
-            <a href="http://www.youtube.com/watch?v=mpXShHdQGcQ"
-                target="_blank">Clique aqui!</a>
-        </p>
-        <p>
-            Este site não hospeda nenhum arquivo. Todo o conteúdo é obtido
-            diretamente do <a href="http://tumblr.com">tumblr.com</a>, e é de
-            responsabilidade de seus autores.
-        </p>
-    </body>
-</html>
-'''
 
 
 def get_urls_by_tag(tag):
@@ -102,12 +60,12 @@ def home():
     if urls is None:
         abort(404)
     url = random.choice(urls)
-    return render_template_string(FRAMESET, url=url)
+    return render_template('frameset.html', url=url)
 
 
 @app.route('/footer')
 def footer():
-    return make_response(FOOTER)
+    return render_template('footer.html')
 
 
 if __name__ == '__main__':
